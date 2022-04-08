c04c7c085b0c001a4951a54b20a2b5ef0fc58999
Emmet Allen <emmetallen@Emmets-MacBook-Pro.local>
2022-04-08 14:16:21 -0400

    Landing Page Started

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,12 +1,24 @@
+/* eslint-disable prettier/prettier */
 import * as React from 'react';
-import {View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Text} from 'react-native-paper';
+import {Title} from 'react-native-paper';
+import { Button } from 'react-native-paper';
+
+const APP_NAME = 'Tag Along';
+
+const CreateEvent = () => (
+  <Button icon="mdiBullhornVariantOutline" mode="contained" onPress={() => console.log('To Event Create Page')}>
+    Create Event
+  </Button>
+);
+
 
 function LandingPage() {
+
   return (
     <SafeAreaView>
-      <Text>Landing Page</Text>
+      <Title>{APP_NAME}</Title>
+      <CreateEvent></CreateEvent>
     </SafeAreaView>
   );
 }
