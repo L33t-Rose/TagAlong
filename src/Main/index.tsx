0a14b73ac5f5c5605e81a687ef37cf504b9fde8d
L33t-Rose <jphillipjean@gmail.com>
2022-04-08 13:24:37 -0400

    Stuff

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import {createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Test from '../Test';
