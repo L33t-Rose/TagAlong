a8dde13f4d6221f119cf9335fd76dcd215867fa9
L33t-Rose <jphillipjean@gmail.com>
2022-04-08 14:07:22 -0400

    Create Event Page

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
 import Main from './src/Main';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const App = () => {
   return (
-    <PaperProvider>
-      <Main />
-    </PaperProvider>
+    <SafeAreaProvider>
+      <PaperProvider>
+        <Main />
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 };
 
